e938eeb834a0680ef512b7c3d9527cf231315068
dutarrr <duzguntutar@gmail.com>
2020-12-09 23:33:19 +0300

    Singleton design pattern

---
+++ 2-Design-Pattern/1-Singleton/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _1_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instanceOne = Singleton.GetInstance();
+            var InstanceTwo = Singleton.GetInstance();
+
+            Console.WriteLine(instanceOne.GetHashCode());
+            Console.WriteLine(InstanceTwo.GetHashCode());
+
+            if(instanceOne.GetHashCode() == InstanceTwo.GetHashCode())
+            System.Console.WriteLine("Bu iki obje eşittir..");
+
+            Console.ReadKey();
+
+        }
+    }
+
+     class Singleton
+    {
+        private static Singleton Instance;
+
+        protected Singleton(){}
+
+        public static Singleton GetInstance()
+        {
+            if(Instance == null)
+                Instance = new Singleton();
+            return Instance;
+        }
+    }
+    
+}
+
+
