44905eab337b63ec66d78667e559d850b5f1287d
dutarrr <duzguntutar@gmail.com>
2020-12-10 17:51:49 +0300

    car builder

--- 2-Design-Pattern/2-Builder/Program.cs
+++ 2-Design-Pattern/2-Builder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace _2_Builder
 {
@@ -8,5 +9,66 @@ namespace _2_Builder
         {
             Console.WriteLine("Hello World!");
         }
+    }
+
+
+    abstract class CarBuilder
+    {
+        protected Car Car;
+        public abstract void BuildModel();
+        public abstract void BuildColor();
+        public abstract void BuildEngine();
+
+    }
+    class MercedesBuilder : CarBuilder
+    {
+        public MercedesBuilder()
+        {
+            Car = new Car("Mercedes");
+        }
+
+        public override void BuildModel()
+        {
+            Car.Model = "C";
+        }
+
+        public override void BuildColor()
+        {
+            Car.Color = "Blue";
+        }
+
+        public override void BuildEngine()
+        {
+            Car.Engine = "2.0";
+        }
+    }
+
+
+    class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string Engine { get; set; }
+        public string Year { get; set; }
+
+        public Car(string Model, string Color, string Engine, string Year)
+        {
+            (Model, Color, Engine, Year) = (Model, Color, Engine, Year);
+        }
+
+         public Car(string Brand)
+        {
+            (Brand) = (Brand);
+        }
+
+        public void Show()
+        {
+            System.Console.WriteLine($"Model : {Model}");
+            System.Console.WriteLine($"Color : {Color}");
+            System.Console.WriteLine($"Engine : {Engine}");
+            System.Console.WriteLine($"Year : {Year}");
+        }
+
     }
 }
