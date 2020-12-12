26c6676314deec516003d008ee8da432bb44500f
dutarrr <duzguntutar@gmail.com>
2020-12-12 23:55:34 +0300

    console app created for prototype design pattern

---
+++ 2-Design-Pattern/5-Prototype/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _5_Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
