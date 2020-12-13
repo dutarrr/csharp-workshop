09f93768aa2998b27411a6330e8529285d550f29
dutarrr <duzguntutar@gmail.com>
2020-12-13 15:58:34 +0300

    shallow and deep copy

--- 1-csharp/ShallowAndDeepCopy/Program.cs
+++ 1-csharp/ShallowAndDeepCopy/Program.cs
@@ -6,7 +6,77 @@ namespace ShallowAndDeepCopy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person p1 = new Person();
+            p1.Age = 33;
+            p1.Name = "Düzgün";
+            p1.IdInfo = new IdInfo(1111);
+
+            Person p2 = p1.ShallowCopy();
+
+            Console.WriteLine("Original values of p1 and p2:");
+            Console.WriteLine("   p1 instance values: ");
+            DisplayValues(p1);
+            Console.WriteLine("   p2 instance values:");
+            DisplayValues(p2);
+
+
+            p1.Age = 31;
+            p1.Name = "Murat";
+            p1.IdInfo.IdNumber = 22222;
+            Console.WriteLine("\nValues of p1 and p2 after changes to p1:");
+            Console.WriteLine("   p1 instance values: ");
+            DisplayValues(p1);
+            Console.WriteLine("   p2 instance values:");
+            DisplayValues(p2);
+
+            System.Console.WriteLine("**************************************");
+
+            Person p3 = p1.DeepCopy();
+
+            p1.Name = "Eda";
+            p1.Age = 29;
+            p1.IdInfo.IdNumber = 3333;
+            Console.WriteLine("\nValues of p1 and p3 after changes to p1:");
+            Console.WriteLine("   p1 instance values: ");
+            DisplayValues(p1);
+            Console.WriteLine("   p3 instance values:");
+            DisplayValues(p3);
+        }
+
+        public static void DisplayValues(Person p)
+        {
+            Console.WriteLine("      Name: {0:s}, Age: {1:d}", p.Name, p.Age);
+            Console.WriteLine("      Value: {0:d}", p.IdInfo.IdNumber);
+        }
+    }
+
+    public class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int IdNumber)
+        {
+            this.IdNumber = IdNumber;
+        }
+    }
+
+    public class Person
+    {
+        public int Age;
+        public string Name;
+        public IdInfo IdInfo;
+
+        public Person ShallowCopy()
+        {
+            return (Person)this.MemberwiseClone();
+        }
+
+        public Person DeepCopy()
+        {
+            Person other = (Person)this.MemberwiseClone();
+            other.IdInfo = new IdInfo(IdInfo.IdNumber);
+            other.Name = String.Copy(Name);
+            return other;
         }
     }
 }
