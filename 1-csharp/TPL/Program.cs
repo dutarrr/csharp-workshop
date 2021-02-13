bbb326956a8dc733385958cfa17cde5b32939d98
dutarrr <duzguntutar@gmail.com>
2021-02-13 22:36:03 +0300

    using Task for async

--- 1-csharp/TPL/Program.cs
+++ 1-csharp/TPL/Program.cs
@@ -17,19 +17,22 @@ namespace TPL
         {
             await Task.Run(() =>
             {
-                for (int i = 0; i < 100; i++)
-                {
-                    Console.WriteLine(Thread.CurrentThread.ManagedThreadId + "---- Method 1");
-                    Task.Delay(100).Wait();
-                }
+                // for (int i = 0; i < 100; i++)
+                // {
+                //     Console.WriteLine(Thread.CurrentThread.ManagedThreadId + "---- Method 1");
+                //     Task.Delay(100).Wait();
+                // }
+
+                Task.Delay(4000).Wait();
             });
+            Console.WriteLine("***************1*******************");
         }
         public static void Method2()
         {
             for (int i = 0; i < 25; i++)
             {
                 Console.WriteLine(Thread.CurrentThread.ManagedThreadId + "----- Method 2");
-                Task.Delay(100).Wait();
+                //Task.Delay(100).Wait();
             }
         }
     }
